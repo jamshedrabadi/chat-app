fb44bca4c1ced323d02b3ef7e4d3190ca4ad867a
Jamshed Rabadi <jamshedrabadi@gmail.com>
2023-12-01 20:00:09 -0500

    set up react frontend

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // eslint-disable-next-line no-console
 app.listen(port, console.log(`Server started on port ${port}`));
